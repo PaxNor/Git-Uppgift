dc64375f0a34aea422bf872a1b2d6a6b79d4ab8f
Lexicon Student 1 <Lexicon Student 1@DESKTOP-OF8V11H>
2022-07-07 09:09:49 +0200

    Added new model class

---
+++ Git Uppgift/Models/NewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Git_Uppgift.Models
+{
+    public class NewModel
+    {
+        public int Id { get; set; }
+        public string  Name { get; set; }
+        public string Description { get; set; }
+    }
+}
