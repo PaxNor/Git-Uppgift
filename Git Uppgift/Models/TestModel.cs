3d8949c90d63709977f5a4c7a11a4f356bd8c50a
Lexicon Student 1 <Lexicon Student 1@DESKTOP-OF8V11H>
2022-07-07 09:52:11 +0200

    Comment

--- Git Uppgift/Models/TestModel.cs
+++ Git Uppgift/Models/TestModel.cs
@@ -3,7 +3,11 @@
     public class TestModel
     {
         public int Id { get; set; }
+<<<<<<< HEAD
+        public string Namn { get; set; }
+=======
         public string Git { get; set; }
+>>>>>>> 8c178b79898dde90caef0208448a012e7cbc78bf
 
     }
 }
